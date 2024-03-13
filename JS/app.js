54196271ac97fb450b483bfb8aab5b1ada85d8df
MarinaLasorsa <marina.lasorsa@gmail.com>
2024-03-13 15:24:03 +0100

    ADD: steps in JS comments to create 100 div elements to fill the grid and change the elements' background color when clicked

--- JS/app.js
+++ JS/app.js
@@ -4,3 +4,31 @@ Ogni cella ha un numero progressivo, da 1 a 100.
 Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro 
 ed emetto un messaggio in console con il numero della cella cliccata.*/
+
+//Recuperare elemento bottone play
+
+//Aggiungere un event listener al click del bottone
+
+//Recuperare elemento griglia con querySelector
+
+//Dichiarare variabile lato griglia
+
+//Calcolare variabile numero di celle facendo lato x lato
+
+//Aprire ciclo for che passa il numero delle celle
+
+//Dichiarare numero che andrà dentro alle celle: indice + 1 per non partire da 0
+
+//Creare elemento div (con dentro numero) con createElement
+
+//dargli classe "cell"
+
+//inserire in elemento griglia con append
+
+//Aggiungere un event listener all'elemento div cella
+
+//aggiungi classe bg-dark a elemento cella 
+//(toggle per aggiungerlo e toglierlo al click)
+
+
+
